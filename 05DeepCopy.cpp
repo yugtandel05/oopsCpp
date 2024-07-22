824cff901b48ab8051a46af36ea5d26f29c78091
yugtandel05 <yugtandel05@gmail.com>
2024-07-22 19:19:29 +0530

    deep copy

---
+++ 05DeepCopy.cpp
@@ -0,0 +1,44 @@
+// Deep Copy -> A deep copy, on the hand not only copies the member values but also makes copies of any dynamically allocated memory that the members point to.
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student{
+public:
+    string name;
+    double* cgpaPtr;
+
+    Student(string name, double cgpa){
+        this->name = name;
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
+    }
+
+    Student(Student &obj){
+        this->name = obj.name;
+        cgpaPtr = new double;  //allocating new memory address to our value
+        *cgpaPtr = *(obj.cgpaPtr);
+    }
+
+    void getInfo(){
+        cout<< "name : "<<name<<endl;
+        cout<< "cgpa : "<<*cgpaPtr<<endl;
+    }
+};
+
+int main(){
+    Student s1("rahul kumer", 8.9);
+    Student s2(s1);
+
+    s1.getInfo();
+
+    *(s2.cgpaPtr) = 9.2;
+    s2.name = "Milind";
+    
+    s1.getInfo();
+    s2.getInfo();
+    
+
+    return 0;
+}
